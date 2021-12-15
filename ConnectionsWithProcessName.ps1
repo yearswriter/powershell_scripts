321afe8d696ca3701e8e42eb02791bdb0b84bad4
yearswriter <i@pesheevpavel.ru>
2021-12-16 02:52:00 +0300

    dns resolving ip adresses and helper function

--- ConnectionsWithProcessName.ps1
+++ ConnectionsWithProcessName.ps1
@@ -8,6 +8,17 @@ Set-Variable ProcessOfInterest -Option ReadOnly -Value *iot*,League* # Name(s) f
 Set-Variable Output -Option ReadOnly -Value "./output.xlsx" # Name for output xlsx file
 Set-Variable SheetName -Option ReadOnly -Value "process connections" # Name for output xlsx file
 
+# Helper function to get hostname with Ip
+function GetHost {
+  param ([string]$Ip)
+  try {
+    return [System.Net.Dns]::GetHostByAddress($Ip).Hostname
+  } catch {
+    return "-"
+  }
+}
+$gethostDef = $function:GetHost.ToString() # The whole functions in the -Parallel foreach ehnanigans (5)
+
 # Get all TCP Connections whith our process names
 $Connections = Get-NetTCPConnection `
   -AppliedSetting Internet ` # Filter conections optimised for internet (1)
@@ -22,9 +33,11 @@ $Connections = Get-NetTCPConnection `
 
 # Enriching $Connections object (3)
 $Connections | ForEach-Object -Parallel {
+  $function:GetHost = $using:gethostDef # recreating function from string definition in every single context (5)
   $_ | Add-Member -NotePropertyName Ping -NotePropertyValue ((Test-Connection -TargetName $_.RemoteAddress -IPv4 -Count 1).Latency)
   $_ | Add-Member -NotePropertyName OwningProcessName -NotePropertyValue ((Get-Process -Id $_.OwningProcess).Name)
   $_ | Add-Member -NotePropertyName CommandLine -NotePropertyValue((Get-Process -Id $_.OwningProcess).CommandLine)
+  $_ | Add-Member -NotePropertyName Hostname -NotePropertyValue(GetHost $_.RemoteAddress)
 }
 
 # Outputing to STDIO with custom table formatting
@@ -33,7 +46,8 @@ $Connections | Format-Table `
   @{Label = "LocalPort";Expression = {[int]($_.LocalPort)}}, `
   @{Label = "RemoteAddress";Expression = {[string]($_.RemoteAddress)}}, `
   @{Label = "Port";Alignment = "Left";Expression = {[int]($_.RemotePort)}}, `
-  @{Label = "Ping";Expression = {[int]$_.Ping}}
+  @{Label = "Ping";Expression = {[int]$_.Ping}}, `
+  @{Label = "Hostname";Expression = {[string]($_.Hostname)}}
 
 # Outputing to excel using ImportExcel module (4)
 $Connections | Export-Excel $Output -WorksheetName $SheetName -TitleBold -AutoSize -FreezeTopRow -AutoFilter
@@ -43,3 +57,4 @@ $Connections | Export-Excel $Output -WorksheetName $SheetName -TitleBold -AutoSi
 #   2. Custom output tables https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/format-table?view=powershell-7.2
 #   3. Adding custom properties and methods to PS object https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/add-member?view=powershell-7.2
 #   4. help export-excel
+#   5. Powershell have problems with function pasing https://stackoverflow.com/questions/61273189/how-to-pass-a-custom-function-inside-a-foreach-object-parallel
