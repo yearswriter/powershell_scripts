4cd7bc95060c60b8d207c0ae933e498bde54e00f
yearswriter <i@pesheevpavel.ru>
2022-01-07 11:53:14 +0300

    Just a better way to do that as cmdlet

--- Start-Split4WindowsTerminal.ps1
+++ Start-Split4WindowsTerminal.ps1
@@ -1,9 +1,8 @@
-function Start-Split4WindowsTerminal {
-    param(
-        $command1 = "pwsh",
-        $command2 = "pwsh",
-        $command3 = "pwsh",
-        $command4 = "pwsh"
-        )
-    Start-Process wt -ArgumentList "new-tab $command1; split-pane -H $command2; mf up split-pane $command3; mf down split-pane $command4" -passthru
-}
+param(
+    $command1 = "pwsh",
+    $command2 = "pwsh",
+    $command3 = "pwsh",
+    $command4 = "pwsh"
+    )
+
+Start-Process wt -ArgumentList "new-tab $command1; split-pane -H $command2; mf up split-pane $command3; mf down split-pane $command4" -passthru
