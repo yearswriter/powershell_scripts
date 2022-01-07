fa91677fcc2055d999a77d02f53a1dbc03bf42fc
yearswriter <i@pesheevpavel.ru>
2022-01-07 12:35:44 +0300

    removed extra space

--- Start-Split4WindowsTerminal.ps1
+++ Start-Split4WindowsTerminal.ps1
@@ -1,8 +1,8 @@
 param(
-    $command1 = "pwsh",
-    $command2 = "pwsh",
-    $command3 = "pwsh",
-    $command4 = "pwsh"
-    )
+  $command1 = "pwsh",
+  $command2 = "pwsh",
+  $command3 = "pwsh",
+  $command4 = "pwsh"
+)
 
 Start-Process wt -ArgumentList "new-tab $command1; split-pane -H $command2; mf up split-pane $command3; mf down split-pane $command4" -passthru
