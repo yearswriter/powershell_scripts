4ca8f522c58ebb1e56e2e69644b722c27b12d46c
yearswriter <i@pesheevpavel.ru>
2022-01-11 16:26:45 +0300

    TODO for future

--- CimInstancesCuriosities.ps1
+++ CimInstancesCuriosities.ps1
@@ -1,2 +1,5 @@
 Get-CimInstance -Namespace OpenHardwareMonitor -ClassName __Namespace
 Get-CimInstance -Namespace Root\OpenHardwareMonitor -Class sensor
+
+# TODO: explore https://www.reddit.com/r/PowerShell/comments/pjvoxm/get_cpu_temperature_wo_wmi/
+# Add-Type -Path .\OpenHardwareMonitorLib.dll
