f7434ebc44045f2d6599eecf3d7168f8a2c324e6
yearswriter <i@pesheevpavel.ru>
2022-01-13 19:56:25 +0300

    Shoddy script screamming when intrenet is out and when it is back again

---
+++ Get-DisconnectTime.ps1
@@ -0,0 +1,44 @@
+
+Start-Job -Name InternetGuard -ScriptBlock {
+  $Voice = New-Object -ComObject SAPI.SPVoice
+  $Voice.Voice = $Voice.GetVoices()[0]
+  [void] $Voice.Speak('Запускаю наблюдателя за интернет-соединением.')
+  $t = Test-NetConnection
+  $internet = $t.PingSucceeded
+  Do {
+    Write-Host $objNotifyIcon.Visible
+    while ($internet) {
+      Start-Sleep -s 900
+      $internet = (Test-NetConnection).PingSucceeded
+    }
+    [void] $Voice.Speak('Проблемы соединения! Запускаю ждуна рабочего подключения.')
+    while (-Not $internet) {
+      Start-Sleep -s 5
+      $internet = (Test-NetConnection).PingSucceeded
+    }
+    [void] $Voice.Speak('Интернет появился!')
+  } While ($True)
+}
+
+try{
+  [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+  $Voice = New-Object -ComObject SAPI.SPVoice
+  $Voice.Voice = $Voice.GetVoices()[0]
+  function CloseGuard {
+    [void] $Voice.Speak('Останавливаю наблюдателя.')
+    $objNotifyIcon.Visible = $False
+    $objNotifyIcon.Dispose()
+    Get-job -Name InternetGuard | Remove-Job -Force
+  }
+
+  $objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon
+  $objNotifyIcon.Icon =  [System.Drawing.Icon]::ExtractAssociatedIcon("C:\Windows\System32\telnet.exe")
+  $objNotifyIcon.add_DoubleClick({CloseGuard})
+  $objNotifyIcon.Text = "Наблюдатель за интернетом"
+  $objNotifyIcon.Tag = "InternetGuard."
+  $objNotifyIcon.Visible = $True
+  Get-Job -Name InternetGuard | Wait-Job
+} finally {
+  $objNotifyIcon.Visible = $False
+  $objNotifyIcon.Dispose()
+}
